b6845cfc8da0a40d115ff7444e3b0140b79205ce
basilia TOMEHO <basiliatomeho@gmail.com>
2025-06-03 00:36:45 +0900

    Update Dataset.py

--- streampages/Dataset.py
+++ streampages/Dataset.py
@@ -2,26 +2,34 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import random
-from datetime import datetime
 
-vehicles_in = np.random.randint(100, size=(24))
-vehicles_out = np.random.randint(100, size=(24))
+
+vehicles_in = np.random.randint(1000, size=(24))
+vehicles_out = np.random.randint(1000, size=(24))
 chart_data = pd.DataFrame(
     {
+        "Timestamp": pd.date_range(start="2025-05-25", periods=24, freq='H'),
         "Hours": list(range(24)),
         "Vehicles_In": vehicles_in,
         "Vehicles_Out": vehicles_out
     }
  )
 
+hours = pd.date_range(end=pd.Timestamp.now(), periods=7 * 24, freq='H')  # last 7 days
+vehicle_types = ['Car', 'Truck', 'Bus', 'Motorcycle']
+data = pd.DataFrame({
+        'Timestamp': np.tile(hours, len(vehicle_types)),
+        'Vehicle Type': np.repeat(vehicle_types, len(hours)),
+        'Count': np.random.poisson(lam=60, size=len(hours) * len(vehicle_types)),
+})
+data['Day'] = data['Timestamp'].dt.day_name()
+data['Hour'] = data['Timestamp'].dt.hour
 
 
 daysofweek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 days = np.tile(daysofweek,1000)
 Count_in = np.random.randint(500, size=(7000))
 Count_out = np.random.randint(500, size=(7000)) 
-
-
 dataset = pd.DataFrame(
     {
         "Days": days,
